82f84a03530c51bbe5aa3f7f56587c77498dd518
code_quest <codequest.info@gmail.com>
2020-06-09 03:00:11 +0200

    Updates

---
+++ precache-manifest.832e4e975093d3126c1ec34c86ccebb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1868dd0d445d3ab2fdd9fecd83837b11",
+    "url": "/ReduxReactBasicApp/index.html"
+  },
+  {
+    "revision": "46992bf975f295b713d9",
+    "url": "/ReduxReactBasicApp/static/css/main.c9b7e7ef.chunk.css"
+  },
+  {
+    "revision": "444c856787c2f566d795",
+    "url": "/ReduxReactBasicApp/static/js/2.282510db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReduxReactBasicApp/static/js/2.282510db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46992bf975f295b713d9",
+    "url": "/ReduxReactBasicApp/static/js/main.6a9f89e1.chunk.js"
+  },
+  {
+    "revision": "5c3116c73d5f480e3a5f",
+    "url": "/ReduxReactBasicApp/static/js/runtime-main.4b2bbad5.js"
+  }
+]);
